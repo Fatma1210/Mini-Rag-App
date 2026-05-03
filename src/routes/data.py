ede617a69912ce662492c6b37b21e40ee65a3aca
fatma1210 <fatmaibrahim1210@gmail.com>
2026-05-03 20:20:44 +0300

    feat: add file processing API with chunking, text and metadata extraction

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI , UploadFile , Depends , APIRouter , status
 from fastapi.responses import JSONResponse
 from helpers.config import Settings , get_settings
-from controllers import DataController , ProjectController
+from controllers import DataController , ProjectController , ProcessController
 from models import ResponseSignal
 import aiofiles
 import os
 import logging
+from .schemes.data import ProcessRequest
 
 logger = logging.getLogger("uvicorn.error")
 data_router = APIRouter(
@@ -57,3 +58,39 @@ async def upload_file(project_id: str , file: UploadFile ,
                         }   
                         
                         )
+
+
+@data_router.post("/process/{project_id}")
+async def process_file(project_id: str , request: ProcessRequest):
+    
+    file_id = request.file_id
+    chunk_size = request.chunk_size
+    overlap_size = request.overlap_size
+
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunnk_size=chunk_size,
+        overlap_size=overlap_size
+        )
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_PROCESSING_FAILED.value
+            }
+        )
+    return file_chunks
+    # return JSONResponse(
+    #     status_code=status.HTTP_200_OK,
+    #     content={
+    #         "signal": ResponseSignal.FILE_PROCESSING_SUCCESS.value,
+    #         "chunks": len(file_chunks) ,
+        
+    #     }
+    # )
