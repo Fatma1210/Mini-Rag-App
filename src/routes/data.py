dd83ea47e77118df21114fa343f9f0ad78d8ab0b
fatma1210 <fatmaibrahim1210@gmail.com>
2026-05-03 15:23:08 +0300

    feat: complete upload file API with file preprocessing
    - Normalize and sanitize uploaded file names
    - Save files under assets/Files/{project_id}/
    - Add file validation (type and size checks)

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI , UploadFile , Depends , APIRouter , status
 from fastapi.responses import JSONResponse
 from helpers.config import Settings , get_settings
-from controllers import DataController
+from controllers import DataController , ProjectController
+from models import ResponseSignal
+import aiofiles
+import os
+import logging
 
+logger = logging.getLogger("uvicorn.error")
 data_router = APIRouter(
     prefix="/api/v1/data",
     tags=["api_v1 , data"],
@@ -25,11 +30,29 @@ async def upload_file(project_id: str , file: UploadFile ,
                             content={
                                 "signal": result_signal
                             }
-                        )
-                    else:
+                        ) 
+                    project_dir_path = ProjectController().get_project_path(project_id=project_id)
+                    file_location = data_controller.generate_unique_file_name(
+                        orig_file_name=file.filename,
+                        project_id=project_id
+                    )
+                    try:
+                        async with aiofiles.open(file_location , "wb") as f:
+                            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE): # read file in chunks
+                                await f.write(chunk)
+                    except Exception as e:
+                        loggor.error(f"Error while Uploading file: {e}")
+
                         return JSONResponse(
-                            status_code=status.HTTP_200_OK,
+                            status_code=status.HTTP_400_BAD_REQUEST,
                             content={
-                                "signal": result_signal
+                                "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
                             }
+                        )
+                    return JSONResponse(
+                        status_code=status.HTTP_200_OK,
+                        content={
+                            "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value
+                        }   
+                        
                         )
