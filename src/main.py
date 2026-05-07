68e4fb5bbb63f77c8da0e25ee5f5a0269d770376
fatma1210 <fatmaibrahim1210@gmail.com>
2026-05-08 02:54:18 +0300

    feat: add LLM provider factory and register models on app startup

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from helpers.config import Settings , get_settings 
 
+from store.llms.LLMSProviderFactory import LLMSProviderFactory
+
 app = FastAPI()
 
 @app.on_event("startup")
@@ -12,6 +14,15 @@ async def startup_db_client():
 
     app.mongodb_conn = AsyncIOMotorClient(settings.MONGODB_URI)
     app.db_client = app.mongodb_conn[settings.MONGODB_DATABASE]
+
+    llm_provider_factory = LLMSProviderFactory(settings)
+    app.generation_client = llm_provider_factory.create(provider = settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
+    app.embedding_client = llm_provider_factory.create(provider = settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(
+        model_id = settings.EMBEDDING_MODEL_ID , 
+        embedding_size = settings.EMBEDDING_MODEL_SIZE
+        )
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
