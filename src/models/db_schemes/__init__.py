4212ecf452b16fb3c0d23528e2592eba4043388d
fatma1210 <fatmaibrahim1210@gmail.com>
2026-05-12 16:38:07 +0300

    fix: resolve incorrect RetrievedDocument schema import in package __init__

--- src/models/db_schemes/__init__.py
+++ src/models/db_schemes/__init__.py
@@ -1,4 +1,4 @@
 from .Project import Project
-from .DataChunk import DataChunk
+from .DataChunk import DataChunk, RetrievedDocument
 from .asset import Asset
-from .DataChunk import RetrievedDocument
+
